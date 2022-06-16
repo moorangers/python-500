a9a217bfc9e2cd872bb8c7c65c65a1bee8683160
MooRangers <atsjan.ch@gmail.com>
2022-06-16 23:10:47 +0700

    Problem  Print String

---
+++ PrintComand/printString.py
@@ -0,0 +1,43 @@
+# # 1
+# print("Hello")
+
+# # 2
+# print("Hello World")
+
+# # 3
+# print("hello_python")
+
+# # 4
+# print("Merry X'mas")
+
+# # 5
+# print('I want to adk you "Why don\'t you drive to work ?"')
+
+# # 6
+# print("\"I don't have a car\"")
+
+# # 7
+# print("You got a new job !? That's so exciting")
+
+# # 8
+# print("สวัสดีวันจันทร์")
+
+# # 9
+# print("ความแตกต่างระหว่างคนเก่งกับคนไม่เก่ง คือ \"การใช้เวลาว่างให้เป็นประโยชน์\"")
+
+# # 10
+# print("/\\/\\/\\")
+
+# # 11
+# print("""
+# a
+# an
+# ant
+# """)
+
+# 12
+print("""
+  *
+* * *
+  *
+""")
